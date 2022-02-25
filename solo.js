b331fc040c6ec9bb813350b71fc643e133b37a23
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-02-25 12:23:14 -0300

    Add files via upload

--- solo.js
+++ solo.js
@@ -1,9 +1,11 @@
-class Solo {
-  constructor(x, y, w, h) {
-    var options = {
-      isStatic: true
+class Solo 
+{
+  constructor(x, y, w,h) 
+  {
+    let options = {
+     isStatic:true
     };
-
+    
     this.body = Bodies.rectangle(x, y, w, h, options);
     this.w = w;
     this.h = h;
@@ -11,12 +13,12 @@ class Solo {
   }
 
   mostrar() {
-    var pos = this.body.position;
+    let pos = this.body.position;
     push();
     rectMode(CENTER);
     noStroke();
-    fill(148, 127, 146);
-    rect(pos.x, pos.y, this.w, this.h);
+    fill(148,127,146);
+    rect(pos.x,pos.y, this.w, this.h);
     pop();
   }
 }
